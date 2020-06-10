d76ab5774541c07e63e54c9544a424ec762bd63b
Isak Evaldsson <isak.evaldsson@outlook.com>
2020-06-10 17:53:13 +0200

    Added get,set and append function to ArrayList.

--- myLibs/ArrayList.c
+++ myLibs/ArrayList.c
@@ -1,5 +1,9 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "ArrayList.h"
-#define ARRAYLIST_INITIAL_CAPACITY 10
+
+#define ARRAYLIST_INITIAL_CAPACITY 4
 
 struct ArrayList {
     unsigned int size;
@@ -14,6 +18,30 @@ ArrayList* createArrayList() {
     list->array = (int*) malloc(sizeof(int) * ARRAYLIST_INITIAL_CAPACITY);
 
     return list;
+}
+
+void extend(ArrayList* list)  {
+    unsigned int extendedCapacity = list->capacity*2;
+    list->array = realloc(list->array, extendedCapacity);
+    assert(list->array);
+    list->capacity = extendedCapacity;
+}
+
+void set(ArrayList* list, unsigned int index, int value) {
+    assert(index < list->size);
+    list->array[index] = value;
+}
+
+int get(ArrayList* list, unsigned int index) {
+    assert(index < list->size);
+    return list->array[index];
+}
+
+void append(ArrayList* list, int value){
+    if(list->size + 1 > list->capacity) {
+        extend(list);
+    }
+    list->array[list->size++] = value;
 }
 
 void destroyArrayList(ArrayList* list) {
